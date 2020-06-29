da9daa15269ca46be8f34b38cf9c848cb84d5898
Ruixiao6 <rui_xiao6@163.com>
2020-06-29 21:53:45 +0800

    这是github使用第一次测试

---
+++ test.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(1300, 800)
+    w.move(120, 50)
+    w.setWindowTitle('去你妈的')
+    w.show()
+    
+    sys.exit(app.exec_())
